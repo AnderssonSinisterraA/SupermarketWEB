94c7598cadfd245902272f7cb6738d667fe2c574
Andersson Sinisterra Angulo <ansa0317@coomeva.com.co>
2024-10-31 14:55:16 -0500

    Produc Delete

---
+++ Pages/Products/Delete.cshtml.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+using System.Threading.Tasks;
+
+namespace SupermarketWEB.Pages.Products
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public DeleteModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Product Product { get; set; } = default;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+
+            if (product != null)
+            {
+                Product = product;
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToPage("./Index");
+        }
+    }
+}
