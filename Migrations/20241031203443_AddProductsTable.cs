f4ce8354a7fdae97cca1bcc44507431163c47b17
Andersson Sinisterra Angulo <ansa0317@coomeva.com.co>
2024-10-31 15:58:33 -0500

    Products

---
+++ Migrations/20241031203443_AddProductsTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SupermarketWEB.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Stork",
+                table: "Products",
+                newName: "Stock");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Stock",
+                table: "Products",
+                newName: "Stork");
+        }
+    }
+}
